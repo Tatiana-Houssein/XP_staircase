e5a94beff1c92d148fc8b757affa8787f1700718
gauthier.delvoye <gauthier.delvoye@kaizen-solutions.net>
2024-03-12 17:32:23 +0100

    feat: first form integrated with formbuilder

--- front_angular/src/app/forms/form-beginning/form-beginning.component.ts
+++ front_angular/src/app/forms/form-beginning/form-beginning.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
 
 @Component({
@@ -7,7 +8,30 @@ import { Router } from '@angular/router';
   styleUrls: ['./form-beginning.component.scss'],
 })
 export class FormBeginningComponent {
-  constructor(private router: Router) {}
+  csps = [
+    'agriculteur exploitant',
+    "commerçant, artisan ou chef d'entreprise",
+    'cadre ou profession intellectuelle supérieure',
+    'profession intermédiaire',
+    'employé ou ouvrier',
+    'étudiant',
+    'ya pas chômeur wesh ?',
+  ];
+
+  genres = ['masquin', 'fémine', 'arbre', 'pas encore regardé sous ma culotte'];
+
+  ageControl = new FormControl('');
+  genreControl = new FormControl('');
+  cspControl = new FormControl('');
+  myForm: FormGroup;
+
+  constructor(private router: Router, private fb: FormBuilder) {
+    this.myForm = this.fb.group({
+      age: this.ageControl,
+      genre: this.genreControl,
+      csp: this.cspControl,
+    });
+  }
 
   sendToInfoFirstTask(): void {
     this.router.navigate(['/info-first-task']);
