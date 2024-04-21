e5dceaeffbc1e834665471cdb01f7d53728e5232
gauthier.delvoye <gauthier.delvoye@kaizen-solutions.net>
2024-04-21 22:07:11 +0200

    fix

--- back/src/constantes.py
+++ back/src/constantes.py
@@ -1,5 +1,5 @@
 from back.src.enum_constantes import StateMetaExperiment, StrategyIA
-from back.src.ia import TableProportionResultExperiment
+from back.src.ia import TableStochastic
 
 AUGMENTATION_LAG = 1
 DIMINUTION_LAG = 2
@@ -8,7 +8,7 @@ TAILLE_POOL_NON_VU = 140  # 160
 TEMPS_EXPOSITION = 0.3  # en milliseconds
 PICKLE_NAME = "experiment.pkl"
 PREFIX_STIMULUS = "visages/visage-"  # tokens/token_
-TABLEAU_PROPORTION_SUR = TableProportionResultExperiment(1, 1000, 1000, 1)
+TABLEAU_PROPORTION_SUR = TableStochastic(1, 1000, 1000, 1)
 DICT_STATE_META_TO_STRATEGY_IA = {
     StateMetaExperiment.first: StrategyIA.sans_ia,
     StateMetaExperiment.second: StrategyIA.sans_fausses_alarmes,
